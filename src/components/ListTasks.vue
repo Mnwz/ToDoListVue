0a6e8ef99654edb9a771d2e433c8799b5304d552
Marcos <marcosorlandork@gmail.com>
2024-02-28 11:33:23 -0300

    adicionado btn dentro do menu

--- src/components/ListTasks.vue
+++ src/components/ListTasks.vue
@@ -1,21 +1,12 @@
 <template>
   <div>
-    <v-text-field
-    clearable
-    label="Add Task"
-    v-model="task.title"
-    @keyup.enter="addTask"
-    >
-  </v-text-field>
+    <v-text-field clearable label="Add Task" v-model="task.title" @keyup.enter="addTask">
+    </v-text-field>
 
     <v-list lines="three" select-strategy="classic">
       <v-list-subheader>General</v-list-subheader>
 
-      <v-list-item
-      v-for="task, index in tasks"
-      :key="index"
-      :value="index"
-      >
+      <v-list-item v-for="(task, index) in tasks" :key="index" :value="index">
         <template v-slot:prepend="{ isActive }">
           <v-list-item-action start>
             <v-checkbox-btn :model-value="isActive"></v-checkbox-btn>
@@ -27,28 +18,31 @@
         <v-list-item-subtitle>
           {{ task.description }}
         </v-list-item-subtitle>
+
+        <template v-slot:append>
+          <v-menu>
+            <template v-slot:activator="{ props }">
+              <v-btn
+              color="purple"
+              icon="mdi-dots-vertical"
+              variant="text" 
+              v-bind="props">
+              </v-btn>
+            </template>
+
+            <v-list>
+              <v-list-item>
+                <v-list-item-title>Editar</v-list-item-title>
+              </v-list-item>
+
+              <v-list-item>
+                <v-list-item-title>Deletar</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </template>
       </v-list-item>
     </v-list>
-     <v-menu>
-        <template v-slot:activator="{ props }">
-          <v-btn
-            color="primary"
-            v-bind="props"
-          >
-            Activator slot
-          </v-btn>
-        </template>
-        <v-list>
-          <v-list-item>
-            <v-list-item-title>Editar</v-list-item-title>
-          </v-list-item>
-        </v-list>
-                <v-list>
-            <v-list-item>
-              <v-list-item-title>Deletar</v-list-item-title>
-            </v-list-item>
-          </v-list>
-      </v-menu>
   </div>
 </template>
 
@@ -61,23 +55,21 @@ const tasks = ref([
     description: "Mais e mais"
   },
   {
-    title: "Toma ",
+    title: "Toma",
     description: "No cu"
   }
 ]);
 
-const task = ref({
-  title: "",
-  description: ""
-});
-const addTask = ()=>{
+const task = ref({});
+
+const addTask = () => {
   tasks.value.push({
     title: task.value.title,
     description: task.value.description
-  })
+  });
   task.value = {
     title: "",
     description: ""
-  }
-}
+  };
+};
 </script>
